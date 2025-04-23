882c8c506d1251a36943aa60c33b97bdee510eb3
Rohit Venkata Sai Dulam <dulam.rohit99@gmail.com>
2025-04-22 23:51:32 -0400

    Update DataHandling.py

--- DataHandling.py
+++ DataHandling.py
@@ -5,50 +5,50 @@ import numpy as np
 import torch
 
 def random_brightness(inp_img):
-	contrast = np.random.rand(1) + 0.5
-	light = np.random.randint(-100, 100)
-	inp_img = contrast * inp_img + light
+    contrast = np.random.rand(1) + 0.5
+    light = np.random.randint(-100, 100)
+    inp_img = contrast * inp_img + light
 
-	return np.clip(inp_img, 0, 255)
+    return np.clip(inp_img, 0, 255)
 
 class SODLoaderAugmentNew(Dataset):
-	def __init__(self):
-    self.inp_path = './AugmentedDUTS384/Image'
-    self.out_path = './AugmentedDUTS384/Mask'
-    self.contour_path = './AugmentedDUTS384/Contour'
+    def __init__(self):
+    	self.inp_path = './AugmentedDUTS384/Image'
+    	self.out_path = './AugmentedDUTS384/Mask'
+    	self.contour_path = './AugmentedDUTS384/Contour'
 
-		self.augment_data = True
-		self.mode = mode
+    	self.augment_data = True
+    	self.mode = mode
 
-		self.inp_files = sorted(glob.glob(self.inp_path + '/*'))
-		self.out_files = sorted(glob.glob(self.out_path + '/*'))
-		self.contour_files = sorted(glob.glob(self.contour_path + '/*'))
+    	self.inp_files = sorted(glob.glob(self.inp_path + '/*'))
+    	self.out_files = sorted(glob.glob(self.out_path + '/*'))
+    	self.contour_files = sorted(glob.glob(self.contour_path + '/*'))
 
-	def __getitem__(self, idx):
-		inp_img = cv.imread(self.inp_files[idx])
-		inp_img = cv.cvtColor(inp_img, cv.COLOR_BGR2RGB)
-		inp_img = inp_img.astype('float32')
+    def __getitem__(self, idx):
+        inp_img = cv.imread(self.inp_files[idx])
+	inp_img = cv.cvtColor(inp_img, cv.COLOR_BGR2RGB)
+	inp_img = inp_img.astype('float32')
 
-		mask_img = cv.imread(self.out_files[idx], 0)
-		mask_img = mask_img.astype('float32')
-		mask_img /= np.max(mask_img)
+	mask_img = cv.imread(self.out_files[idx], 0)
+	mask_img = mask_img.astype('float32')
+	mask_img /= np.max(mask_img)
 
-		contour_img = cv.imread(self.contour_files[idx], 0)
-		contour_img = contour_img.astype('float32')
-		contour_img /= np.max(contour_img)
+   	contour_img = cv.imread(self.contour_files[idx], 0)
+	contour_img = contour_img.astype('float32')
+	contour_img /= np.max(contour_img)
 
-		if self.augment_data:
-			inp_img = random_brightness(inp_img)
+	if self.augment_data:
+	    inp_img = random_brightness(inp_img)
 
-		inp_img /= np.max(inp_img)
-		inp_img = np.transpose(inp_img, axes=(2, 0, 1))
-		inp_img = torch.from_numpy(inp_img).float()
+    	inp_img /= np.max(inp_img)
+    	inp_img = np.transpose(inp_img, axes=(2, 0, 1))
+    	inp_img = torch.from_numpy(inp_img).float()
 
-		mask_img = np.expand_dims(mask_img, axis=0)
+    	mask_img = np.expand_dims(mask_img, axis=0)
 
-		contour_img = np.expand_dims(contour_img, axis=0)
+    	contour_img = np.expand_dims(contour_img, axis=0)
 
-		return inp_img, torch.from_numpy(mask_img).float(), torch.from_numpy(contour_img).float()
+    	return inp_img, torch.from_numpy(mask_img).float(), torch.from_numpy(contour_img).float()
 
-	def __len__(self):
-		return len(self.inp_files)
+    def __len__(self):
+    	return len(self.inp_files)
